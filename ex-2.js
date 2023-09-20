91464a679e29d3d4c6129cec11385982d9af3ec9
pranaiya suwanta <27mar03.jk@gmail.com>
2023-09-20 17:20:07 +0700

    finished assignment

--- ex-2.js
+++ ex-2.js
@@ -12,7 +12,23 @@ const employees = [
   },
 ];
 // Start coding here
-let alex;
-let alexHobbies;
-let alexAndJamesAge;
+let alex = employees[1].name;
+employees[1].hobbies.push("Watching basketball");
+let alexHobbies = employees[1].hobbies;
+const jamesAge = employees[0].age;
+const alexAge = employees[1].age;
+let alexAndJamesAge = jamesAge + alexAge;
 const updateEmployees = employees;
+updateEmployees.push({
+  name: "Alicia",
+  age: 29,
+  hobbies: ["Shopping", "Reading novels"],
+});
+updateEmployees.push({
+  name: "Kody",
+  age: 19,
+  hobbies: ["Computer games", "Wakeboard"],
+});
+console.log(updateEmployees);
+updateEmployees.pop();
+console.log(updateEmployees);
